7861e0911608e282fe5cbfbfd0079d04f5cc6c0b
npq-thien <thiennguyen3132002@gmail.com>
2023-12-26 19:06:18 +0700

    auth and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
-import './globals.css'
+import { Route, Routes } from 'react-router-dom';
+
+import './globals.css';
+import { SigninForm, SignupForm } from './_auth/forms';
+import { Home } from './_root/pages';
+import AuthLayout from './_auth/AuthLayout';
+import RootLayout from './_root/RootLayout';
 
 const App = () => {
-    return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+    return (
+        <main className="flex h-screen">
+            <Routes>
+                {/* Public routes */}
+                <Route element={<AuthLayout />}>
+                    <Route path="/sign-in" element={<SigninForm />} />
+                    <Route path="/sign-up" element={<SignupForm />} />
+                </Route>
+
+                {/* Private routes */}
+                <Route element={<RootLayout />}>
+                    <Route index element={<Home />} />
+                </Route>
+            </Routes>
+        </main>
+    );
 };
 
 export default App;
