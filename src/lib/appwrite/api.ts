3e87f10ad4d9e36337a59322420563de5eb3f4b6
npq-thien <thiennguyen3132002@gmail.com>
2024-02-27 21:40:33 +0700

    UI: topBar

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -73,3 +73,13 @@ export async function getCurrentAccount() {
     console.log(error);
   }
 }
+
+export async function signOutAccount() {
+  try {
+    const session = await account.deleteSession('current');
+
+    return session;
+  } catch (error) {
+    console.log(error);
+  }
+}
--- src/lib/react-query/queriesAndMutation.ts
+++ src/lib/react-query/queriesAndMutation.ts
@@ -1,6 +1,6 @@
 import { INewUser } from '@/types';
 import { useQuery, useMutation, useQueryClient, useInfiniteQuery } from '@tanstack/react-query';
-import { createUserAccount, signInAccount } from '../appwrite/api';
+import { createUserAccount, signInAccount, signOutAccount } from '../appwrite/api';
 
 export const useCreateAccount = () => {
   return useMutation({
@@ -11,5 +11,11 @@ export const useCreateAccount = () => {
 export const useSignInAccount = () => {
   return useMutation({
     mutationFn: (user: { email: string; password: string }) => signInAccount(user),
+  });
+};
+
+export const useSignOutAccount = () => {
+  return useMutation({
+    mutationFn: signOutAccount,
   });
 };
