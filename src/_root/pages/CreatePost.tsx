0bb9e1c7f9994ac4b54e74c15294c4d77518e781
npq-thien <thiennguyen3132002@gmail.com>
2024-02-29 22:54:18 +0700

    creating UI for Create Post (2:55)

--- src/_root/pages/CreatePost.tsx
+++ src/_root/pages/CreatePost.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import PostForm from "@/components/forms/PostForm";
 
 const CreatePost = () => {
   return (
-    <div>CreatePost</div>
-  )
-}
+    <div className="flex flex-1">
+      <div className="common-container">
+        <div className="flex-start gap-2">
+          <img 
+            src="public/assets/icons/add-post.svg" 
+            alt="add" 
+            width={36}
+            height={36} 
+          />
+          <p className="h3-bold md:h2-bold text-left">Create Post</p>
+        </div>
+      <PostForm />
+      </div>
+    </div>
+  );
+};
 
-export default CreatePost
+export default CreatePost;
